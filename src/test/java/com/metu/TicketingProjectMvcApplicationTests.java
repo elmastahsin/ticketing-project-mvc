dfca874fe794d8b7221fee77b1f94f09652aef64
tahsinelmas <elmas.tahsin@metu.edu.tr>
2023-04-05 04:51:04 +0300

    TicketingProjectMvcApp created

---
+++ src/test/java/com/metu/TicketingProjectMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingProjectMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
