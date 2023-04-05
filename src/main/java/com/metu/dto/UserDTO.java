70b98eac28837dd460e16be0280c2feb349b9ad2
tahsinelmas <elmas.tahsin@metu.edu.tr>
2023-04-05 05:25:33 +0300

    TicketingProjectMvcApp created

---
+++ src/main/java/com/metu/dto/UserDTO.java
@@ -0,0 +1,22 @@
+package com.metu.dto;
+
+import com.metu.entity.Role;
+import com.metu.enums.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String password;
+    private boolean enabled;
+    private String phone;
+    private RoleDTO role;
+    private Gender gender;
+}
