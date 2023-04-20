6613346846cbbfef4706d2faec0d241d758021d0
tahsinelmas <elmas.tahsin@metu.edu.tr>
2023-04-20 03:57:20 +0300

    TicketingProjectMvcApp updated

---
+++ src/main/java/com/metu/converter/RoleDTOConverter.java
@@ -0,0 +1,14 @@
+package com.metu.converter;
+
+import com.metu.dto.RoleDTO;
+import org.springframework.core.convert.converter.Converter;
+
+
+public class RoleDTOConverter implements Converter<String, RoleDTO> {
+
+    @Override
+    public RoleDTO convert(String source) {
+        return null;
+    }
+}
+
